52299c6e37d49c152c73a5ed318e0ef657b9e96b
Wouter <wouter.opsommer@student.hogent.be>
2019-10-23 16:40:45 +0100

    init factoryapi

---
+++ Factory/Factory/Models/MachineType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryApi.Models
+{
+    public class MachineType
+    {
+    }
+}
